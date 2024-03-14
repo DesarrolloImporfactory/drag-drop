a256cbf70c66ce12570bd7a0328a53e73c069eb1
Jeimy Jara <jeimyjara@hotmail.com>
2024-03-13 21:24:14 -0500

    cambios

--- resources/views/pagebuilder/category/settings.php
+++ resources/views/pagebuilder/category/settings.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Arr;
+
 $id = $_SERVER["PATH_INFO"];
 if (strpos($id, "/pages") === 0) {
     $id = str_replace("/pages/", "", $id);
@@ -7,16 +9,15 @@ if (strpos($id, "/pages") === 0) {
     $id = str_replace("/iframe", "", $id);
 }
 
-
-
 $imporsuit_db = mysqli_connect("194.163.183.231", 'administrador', '69635201d674bcb6f0897604c7c97cf8', 'drag');
-
+isset($_POST['id']) ? $id = $_POST['page_id'] : $id = $id;
 $url_origen = "select origen_url from pagebuilder__pages where id = '$id'";
 $result = mysqli_query($imporsuit_db, $url_origen);
 $origen = mysqli_fetch_assoc($result);
 
 
 $archivo_tienda =  $origen['origen_url'] . '/sysadmin/vistas/db1.php';
+
 $archivo_destino_tienda = "../db_destino_guia.php";
 $contenido_tienda = file_get_contents($archivo_tienda);
 $get_data = json_decode($contenido_tienda, true);
@@ -27,11 +28,10 @@ if (file_put_contents($archivo_destino_tienda, $contenido_tienda) !== false) {
     $base_d = $get_data['DB_NAME'];
     // Conexión a la base de datos de la tienda, establece la hora -5 GTM
     date_default_timezone_set('America/Guayaquil');
-    $tienda_db = mysqli_connect("194.163.183.231", $user_d, $pass_d, $base_d);
-    if (!$conexion) {
+    $tienda_db = mysqli_connect("158.220.107.176", $user_d, $pass_d, $base_d);
+    if (!$tienda_db) {
         die("Connection failed: " . mysqli_connect_error());
     }
-    return $conexion;
 } else {
     echo "Error al copiar el archivo";
 }
@@ -39,12 +39,17 @@ if (file_put_contents($archivo_destino_tienda, $contenido_tienda) !== false) {
 $sql = "SELECT * FROM `lineas`";
 $result = mysqli_query($tienda_db, $sql);
 $categories = [];
-
 while ($row = mysqli_fetch_assoc($result)) {
-    $categories[] = $row;
+    $categories[$row['nombre_linea']] = $row['nombre_linea'];
 }
 
-print_r($categories);
+$categoria_link = "categoria.php?id_cat=";
+
+
+$options =  $categories;
+
+
+mysqli_close($tienda_db);
 
 
 // realiza una peticion a la base de datos para obtener las categorias
@@ -52,8 +57,25 @@ print_r($categories);
 
 return [
     'id' => 'category',
-    'name' => __('Carousel de imagenes'),
+    'name' => __('Categoria'),
     'icon' => '<i class="bx bxs-category"></i>',
     'tab' => "Ecommerce",
-    'fields' => []
+    'fields' => [
+        [
+            'id' => 'categoriaa', // 'id' => 'cod_producto',
+            'name' => 'categoriaa',
+            'type' => 'select',
+            'label_title' => __('Selecciona la categoria'),
+            'options' => $options
+        ],
+        [
+            'id' => 'link_category',
+            'name' => 'link_category',
+            'type' => 'text',
+            'label_title' => __('Enlace'),
+
+            'value' => $origen['origen_url'] . $categoria_link
+        ]
+
+    ]
 ];
